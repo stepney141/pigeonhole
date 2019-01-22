0c43868b58dbd8ba87f10b4c630784f3d937b45e
stepney141 <stepney141andi.c.i@gmail.com>
2019-01-23 04:35:01 +0900

    updated scripts

--- wget-downloader-url012.sh
+++ wget-downloader-url012.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
 
+# while 
 read -r input
-
-wget \
--e robots=off \
---no-parent \
---mirror \
---wait=3 \
---random-wait \
---page-requisites \
---convert-links \
---trust-server-names \
---no-verbose \
---retry-on-http-error=408,500,502,503,504 \
---retry-connrefused \
---output-file=test/wgetlog012-$input.txt \
---input-file=edit-url/url012/url012-$input.txt
+# do
+    nohup wget \
+    -e robots=off \
+    --no-parent \
+    --mirror \
+    --wait=3 \
+    --random-wait \
+    --page-requisites \
+    --convert-links \
+    --trust-server-names \
+    --no-verbose \
+    --adjust-extension \
+    # --convert-file-only \
+    --retry-on-http-error=408,500,502,503,504 \
+    # --retry-connrefused \
+    --output-file=wgetlog/wgetlog012-$input.txt \
+    --input-file=url012/url012-$input.txt &
+# done
--- wget_downloader.sh
+++ wget_downloader.sh
@@ -1,14 +1,15 @@
-nohup wget \
--e robots=off \
---no-parent \
---mirror \
---wait=3 \
---random-wait \
---page-requisites \
---convert-links \
---trust-server-names \
---no-verbose \
---retry-on-http-error=408,500,502,503,504 \
---retry-connrefused \
---output-file=test/wgetlog001.txt \
---input-file=test/urltest.txt &
+
+    nohup wget \
+    -e robots=off \
+    --no-parent \
+    --mirror \
+    --wait=3 \
+    --random-wait \
+    --page-requisites \
+    --convert-links \
+    --trust-server-names \
+    --no-verbose \
+    --retry-on-http-error=408,500,502,503,504 \
+    --retry-connrefused \
+    --output-file=wgetlog-$input/wgetlog001.txt \
+    --input-file=url012/url012-$input.txt &
