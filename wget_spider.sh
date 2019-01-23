c26679c9be968f4c211f29266f90a0397df5cedc
stepney141 <stepney141andi.c.i@gmail.com>
2019-01-24 03:13:14 +0900

    updated scripts

--- wget_spider.sh
+++ wget_spider.sh
@@ -1,4 +1,3 @@
 ## foo.txt内のURLの中から生きてるものだけを抽出後、bar.txtに出力
-nohup cat url002.txt |
-wget --spider --no-verbose --output-file=url002-checked.txt --input-file=- &
+nohup cat dump.txt |wget --spider --no-verbose --retry-on-http-error=408,500,502,503,504 --output-file=dump-checked.txt --input-file=- &
 grep "200 OK" /home/stepney141/pigeonhole/url002a.txt > /home/stepney141/pigeonhole/url002-checked.txt
